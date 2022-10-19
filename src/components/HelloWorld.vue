66666eac30f344fc8d2aeceeb276ff22705b5120
Cybaster <1219925930@qq.com>
2022-10-19 21:41:17 +0800

    change something

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,54 +14,54 @@ export default {
       map: null,
       polyline: null,
       timer: null,
-      points:[],
+      points: [],
       T: 0
     }
   },
   mounted() {
     this.map = new this.$BMapGL.Map("container")
 
-    var point = new this.$BMapGL.Point(116.403119,39.914271)
+    const point = new this.$BMapGL.Point(116.403119, 39.914271);
     this.map.centerAndZoom(point, 14)
     this.map.enableScrollWheelZoom(true)
-    this.map.setHeading(0);   //设置地图旋转角度
-    this.map.setTilt(0);       //设置地图的倾斜角度
+    this.map.setHeading(40);   //设置地图旋转角度
+    this.map.setTilt(45);       //设置地图的倾斜角度
 
-    var scaleCtrl = new this.$BMapGL.ScaleControl();  // 添加比例尺控件
+    const scaleCtrl = new this.$BMapGL.ScaleControl();  // 添加比例尺控件
     this.map.addControl(scaleCtrl);
-    var zoomCtrl = new this.$BMapGL.ZoomControl();  // 添加缩放控件
+    const zoomCtrl = new this.$BMapGL.ZoomControl();  // 添加缩放控件
     this.map.addControl(zoomCtrl);
-    var cityCtrl = new this.$BMapGL.CityListControl();  // 添加城市列表控件
+    const cityCtrl = new this.$BMapGL.CityListControl();  // 添加城市列表控件
     this.map.addControl(cityCtrl);
 
     let _this = this
-    this.map.addEventListener('click', function(e) {
+    this.map.addEventListener('click', function (e) {
       _this.points.push([e.latlng.lng, e.latlng.lat])
       _this.map.addOverlay(new _this.$BMapGL.Marker(new _this.$BMapGL.Point(e.latlng.lng, e.latlng.lat)))
     })
 
-    var txtMenuItem = [
+    const txtMenuItem = [
       {
         text: '运行',
         callback: () => {
           this.renderMarker()
-          this.T = 1.1 * this.points.length
+          this.T = 10.0 * this.points.length
           if (this.timer) {
             clearInterval(this.timer)
           }
           this.timer = setInterval(this.optimize, 100)
         }
       }
-    ]
-    var menu = new this.$BMapGL.ContextMenu();
-    for (var i = 0; i < txtMenuItem.length; i++) {
+    ];
+    const menu = new this.$BMapGL.ContextMenu();
+    for (let i = 0; i < txtMenuItem.length; i++) {
       menu.addItem(new this.$BMapGL.MenuItem(
-          txtMenuItem[i].text,
-          txtMenuItem[i].callback,
-          {
-            width: 300,
-            id: 'menu' + i
-          }
+              txtMenuItem[i].text,
+              txtMenuItem[i].callback,
+              {
+                width: 300,
+                id: 'menu' + i
+              }
           )
       )
     }
@@ -76,7 +76,7 @@ export default {
 
       this.polyline = new this.$BMapGL.Polyline(
           map1,
-          {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5}
+          {strokeColor: "blue", strokeWeight: 2, strokeOpacity: 0.5}
       )
       this.map.addOverlay(this.polyline)
     },
@@ -94,8 +94,7 @@ export default {
       const L = 100
       const K = 0.99
 
-      // let len = this.lengthSum(this.points)
-      if (this.T > 0.001) {
+      if (this.T > 0.0001) {
         for (let i = 0; i < L; i++) {
           let len1 = this.lengthSum(this.points)
 
@@ -106,9 +105,12 @@ export default {
             p2 = Math.floor(Math.random() * n)
           }
           let temp_points = [...this.points]
-          let temp = temp_points[p1]
-          temp_points[p1] = temp_points[p2]
-          temp_points[p2] = temp
+          let temp1 = temp_points[p1]
+          // let temp2 = temp_points[p2]
+          // temp_points[p1] = temp_points[p2]
+          // temp_points[p2] = temp
+          temp_points.splice(p1, 1)
+          temp_points.splice(p2, 0, temp1)
           let len2 = this.lengthSum(temp_points)
 
           let delta_e = len2 - len1
@@ -128,16 +130,16 @@ export default {
       }
       this.clear()
       this.render()
-      console.log(this.lengthSum(this.points))
-      console.log(this.T)
+      // console.log(this.lengthSum(this.points))
+      // console.log(this.T)
     },
     lengthSum(points) {
       let len = 0;
       const n = points.length;
-      for(let i = 0; i< n - 1; i++) {
-        len += Math.sqrt(Math.pow(points[i][0] - points[i+1][0], 2) + Math.pow(points[i][1] - points[i+1][1], 2))
+      for (let i = 0; i < n - 1; i++) {
+        len += Math.sqrt(Math.pow(points[i][0] - points[i + 1][0], 2) + Math.pow(points[i][1] - points[i + 1][1], 2))
       }
-      len += Math.sqrt(Math.pow(points[n-1][0] - points[0][0], 2) + Math.pow(points[n-1][1] - points[0][1], 2))
+      len += Math.sqrt(Math.pow(points[n - 1][0] - points[0][0], 2) + Math.pow(points[n - 1][1] - points[0][1], 2))
       return len
     }
   }
